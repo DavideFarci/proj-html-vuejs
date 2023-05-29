8de054058f4ef7f38996d993383f4fcbe2e1b1e2
DavideFarci <yank92.df@gmail.com>
2023-05-29 17:13:26 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import AppFooter from "./components/AppFooter.vue";
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+
+export default {
+  components: { AppHeader, AppMain, AppFooter },
+};
+</script>
+
+<template>
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
+</template>
+
+<style lang="scss">
+@import "../node_modules/bootstrap/scss/bootstrap";
+</style>
