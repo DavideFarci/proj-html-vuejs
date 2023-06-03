242e5b015d89110e037df9fc4c0fee4732994f05
DavideFarci <yank92.df@gmail.com>
2023-06-03 12:18:29 +0200

    add bootstrap one more time

--- src/main.js
+++ src/main.js
@@ -3,4 +3,7 @@ import App from "./App.vue";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "./style.css";
 
+// Import all of Bootstrap's JS
+import * as bootstrap from "bootstrap";
+
 createApp(App).mount("#app");
