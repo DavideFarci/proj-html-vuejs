ba92dfc8ccd79638df635aecf164400011accaf0
DavideFarci <yank92.df@gmail.com>
2023-06-04 12:54:56 +0200

    fix error in appCardDouble

--- src/components/AppCardDouble.vue
+++ src/components/AppCardDouble.vue
@@ -3,9 +3,9 @@ export default {
   props: {
     dataCard: Array,
   },
-  filters: {
-    limit(dataCard, count) {
-      return dataCard.slice(0, count);
+  methods: {
+    getLimitedArray(dataCard) {
+      return dataCard.slice(0, 2);
     },
   },
 };
@@ -13,9 +13,9 @@ export default {
 
 <template>
   <div class="row">
-    <div class="col-6" v-for="(card, i) in dataCard | limit(2)" :key="i">
+    <div class="col-6" v-for="(card, i) in getLimitedArray(dataCard)" :key="i">
       <div class="content">
-        <img :src="`../assets/img/${card.image}`" :alt="card.image" />
+        <img :src="`src/assets/img/${card.image}`" :alt="card.image" />
         <div class="yellow_strip">
           <span class="strip_content">{{ card.title }}</span>
         </div>
@@ -49,6 +49,38 @@ export default {
     img {
       width: 100%;
     }
+  }
+}
+
+.yellow_strip {
+  background-color: #f9aa01;
+  width: 10rem;
+  height: 1.5rem;
+  transform: skew(-20deg);
+  transform-origin: top right;
+  overflow: hidden;
+  position: absolute;
+  right: 8rem;
+  top: 6rem;
+  text-align: center;
+  .strip_content {
+    font-size: 1.6em;
+    color: #242225;
+    text-align: center;
+    font-weight: bolder;
+    position: relative;
+    bottom: 6px;
+  }
+}
+.title_content {
+  font-size: 1.6em;
+  font-weight: bolder;
+  color: white;
+  position: absolute;
+  top: 8rem;
+  right: 6rem;
+  & > * {
+    letter-spacing: 2px;
   }
 }
 </style>
